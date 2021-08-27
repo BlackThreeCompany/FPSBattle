0052ef8f768fcb3a32e4f5be94739683eb620742
junsangy1234 <junsangy1234@gmail.com>
2021-08-27 15:25:45 +0900

    ..

--- FPSGame_NEW/Assets/Scripts/ActionController.cs
+++ FPSGame_NEW/Assets/Scripts/ActionController.cs
@@ -98,20 +98,18 @@ public class ActionController : MonoBehaviour
                         CantPickUpTime = 0;
                         return;
                     }
-                    if(hitInfo.transform.gameObject.GetComponent<ItemPickUp>().item.itemType == Item.ItemType.Ammo)
-                    {
-                        Debug.Log("총알주움");
-                        theInventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item, 30);
-                    }
-                    else
-                    {
-                        theInventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item);
-                    }
-
+                    theInventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item);
                     Destroy(hitInfo.transform.gameObject);
                     InfoDisappear();
                 }
-                theInventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item);
+                if (hitInfo.transform.gameObject.GetComponent<ItemPickUp>().item.itemType == Item.ItemType.Ammo)
+                {
+                    theInventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item, 30);
+                }
+                else
+                {
+                    theInventory.AcquireItem(hitInfo.transform.GetComponent<ItemPickUp>().item);
+                }
                 Destroy(hitInfo.transform.gameObject);
                 InfoDisappear();
                     
