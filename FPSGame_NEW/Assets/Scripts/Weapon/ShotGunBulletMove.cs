929909a566d451af00c4dd795d7fcc336fd1eeeb
junsangy1234 <junsangy1234@gmail.com>
2021-09-18 17:52:15 +0900

    ShotGun & Sniper

---
+++ FPSGame_NEW/Assets/Scripts/Weapon/ShotGunBulletMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class ShotGunBulletMove : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * StatManager.instance.BulletSpeed);
+    }
+}
